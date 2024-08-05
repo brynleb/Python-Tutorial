9622bf6b9dd8eef039fc307bb1b100d440948745
Brynle Barrett <30622290+brynleb@users.noreply.github.com>
2024-08-05 08:48:39 -0300

    Initial commit

---
+++ MyModule.py
@@ -0,0 +1,13 @@
+### Test module for Modules.py ###
+
+print('Imported MyModule.py...')
+
+test = 'Test String'
+
+def find_index(to_search, target):
+	'''Find the index of a value in a sequence'''
+	for i, value in enumerate(to_search):
+		if value == target:
+			return 1
+
+	return -1
